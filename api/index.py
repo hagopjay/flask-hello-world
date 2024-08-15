4a1e3a781a54ad28daf45f456a29ccefc7525f6b
hagopjay <hagopjay@gmail.com>
2024-08-15 13:09:42 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Hello there World!'
 
 @app.route('/about')
 def about():
-    return 'About'
+    return 'AboutZ'
